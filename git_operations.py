41cc07d3b2ce5b61ec71a56a83e6565c09459259
Kory <jyzhaoseu@163.com>
2025-04-14 14:01:51 +0800

    modify srcipt

--- git_operations.py
+++ git_operations.py
@@ -5,17 +5,17 @@ import subprocess
 subfolders = ['AppFrameThoughts', 'BlogSrc', 'CppLearn', 'CSFundations', 'GoLearn', 'LinuxLearn', 'PythonLearn', 'Readings', 'RustLearn', 'ZImages']
 
 # 定义处理子文件夹的函数
-def process_subfolder(subfolder):
+def process_subfolder(subfolder, remote_branch='HEAD:master'):
     my_note_path = os.path.dirname(os.path.abspath(__file__))
     my_note_sub_path = os.path.join(my_note_path, subfolder)
     original_dir = os.getcwd()
     os.chdir(my_note_sub_path)
     print(f'当前目录: {os.getcwd()}')
     # 检查是否存在 diff
-    result = subprocess.run(['git', 'diff', '--no-ext-diff', '--cached', '--quiet'], capture_output=True)
+    result = subprocess.run(['git', 'diff', '--quiet'], capture_output=True)
     # 检查是否存在未跟踪文件
     status_result = subprocess.run(['git', 'status', '--porcelain'], capture_output=True, text=True)
-    untracked_files = [line for line in status_result.stdout.split('\n') if line.startswith('?? ')]
+    untracked_files = [line for line in status_result.stdout.split('\n') if line.startswith('?? ') or line.startswith('M ')]
     if result.returncode != 0 or untracked_files:
         # 使用 git add 添加更改
         subprocess.run(['git', 'add', '.'])
@@ -27,10 +27,10 @@ def process_subfolder(subfolder):
         # 执行 git commit
         subprocess.run(['git', 'commit', '-m', commit_message])
         # 推送到 HEAD:master
-        subprocess.run(['git', 'push', 'origin', 'HEAD:master'])
+        subprocess.run(['git', 'push', 'origin', remote_branch])
     os.chdir(original_dir)
 
 # 遍历指定子文件夹
 for subfolder in subfolders:
     process_subfolder(subfolder)
-process_subfolder('')
+process_subfolder('', 'main')
